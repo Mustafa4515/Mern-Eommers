4c061c43095116423845e05dec7645b0be496e11
Ghulam Mustafa <teachworld15455@gmail.com>
2023-02-06 16:20:14 +0500

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+       <a href="/">Brand's Factory</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
