a8f3d22707d5211867fc6c248f4ce7c673714fcf
Ghulam Mustafa <teachworld15455@gmail.com>
2023-02-09 22:10:21 +0500

    SigniinScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Badge from 'react-bootstrap/Badge';
 import { useContext } from 'react';
 import { Store } from './screens/Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
@@ -39,8 +40,9 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen/>}/>
+              <Route path="/signin" element={<SigninScreen/>}/>
               <Route path="/" element={<HomeScreen />} />
-              <Route path='/cart' element={<CartScreen/>}/>
             </Routes>
           </Container>
         </main>
