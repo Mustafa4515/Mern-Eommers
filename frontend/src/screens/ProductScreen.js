2a3132c7ecfb9edd71d20a30ff38afba9ac4515f
Ghulam Mustafa <teachworld15455@gmail.com>
2023-02-09 07:10:00 +0500

    error

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -10,6 +10,9 @@ import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -37,17 +40,17 @@ export default function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload:getError(err) });
       }
     };
     fetchData();
   }, [slug]);
 
   return loading ? (
-    <div>loading...</div>
-  ) : error ? (
-    <div>{error}</div>
-  ) : (
+    <LoadingBox/>
+    ) : error ? (
+      <MessageBox variant="danger">{error}</MessageBox>
+    ): (
     <div>
       <Row>
         <Col md={6}>
