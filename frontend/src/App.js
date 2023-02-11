9b6a683cbb793a5ef0ac07fa43d3955e115b79dc
Ghulam Mustafa <teachworld15455@gmail.com>
2023-02-11 11:52:27 +0500

    Create Sign Up Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import { useContext } from 'react';
 import { Store } from './screens/Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import SignupScreen from './screens/SignupScreen';
 import ShippingAddressScreen from "./screens/ShippingAddressScreen"
 function App() {
   const { state,dispatch:ctxDispatch } = useContext(Store);
@@ -73,6 +74,7 @@ const signoutHandler=()=>{
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen/>}/>
               <Route path="/signin" element={<SigninScreen/>}/>
+              <Route path="/signup" element={<SignupScreen/>}/>
               <Route path="/shipping" element={<ShippingAddressScreen/>}/>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
